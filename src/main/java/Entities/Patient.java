60a0b728b6924e91c9c27a57b48cf1a967784a50
Zacks_World <elguazzarzakaria10@gmail.com>
2025-03-10 21:47:24 +0000

    Creation of the class Patient.java

---
+++ src/main/java/Entities/Patient.java
@@ -0,0 +1,26 @@
+package Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Date;
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Date dateNaissance;
+    private boolean malade;
+    private int score;
+}
