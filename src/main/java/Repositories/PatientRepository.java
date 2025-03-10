e5fd77cff6c1ccffbc8517dbc6282f1692307b98
Zacks_World <elguazzarzakaria10@gmail.com>
2025-03-10 22:05:41 +0000

    Creating the PatientRepository interface

---
+++ src/main/java/Repositories/PatientRepository.java
@@ -0,0 +1,8 @@
+package Repositories;
+
+import Entities.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+
+}
