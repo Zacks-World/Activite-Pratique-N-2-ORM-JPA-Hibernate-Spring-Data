cb4dc5f7b73e1c789b29975599fa443429e4216f
Zacks_World <elguazzarzakaria10@gmail.com>
2025-03-10 23:10:25 +0000

    Adding @Repository

--- src/main/java/Repositories/PatientRepository.java
+++
@@ -1,8 +0,0 @@
-package Repositories;
-
-import Entities.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PatientRepository extends JpaRepository<Patient, Long> {
-
-}
